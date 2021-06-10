084b50038d261ced3b9a9c4964d9b4652aeff812
jwmurray <john@gardenway.org>
2021-06-10 08:45:33 -0600

    Update anonymize code.

--- anonymize.py
+++ anonymize.py
@@ -3,6 +3,8 @@
 import argparse
 import hashlib
 import uuid
+from os import makedirs, path
+import os
 from logging import debug
 from logging.config import dictConfig
 
@@ -14,20 +16,83 @@ import binascii, struct
 class Anon:
     def __init__(self):
         self.salt = uuid.uuid4().hex.encode('utf-8')
+        self.patient_id = 1
         debug("salt: {}".format(self.salt))
 
+    # Create an anonymous
     def pseudonym(self, name):
         hashed_name = hashlib.pbkdf2_hmac('sha256', name.encode(), self.salt, 100000)
         # return hashlib.sha512(name.encode('utf-8') + self.salt.encode('utf-8')).hexdigest()
         # hashed_name = binascii.b2a_base64(struct.pack('i', hash(name)))
         return hashed_name
 
+    # Create anonymous .bmp file from .bmp file
+    def anonymize_file(self, input_file_path, output_file_path):
+        debug('Anonymizing:  {} ==> {}'.format(input_file_path, output_file_path))
+        try:
+            input_image = Image.open(input_file_path)
+        except:
+            return
+        a = numpy.array(input_image)
+        a = numpy.delete(a, slice(20), axis=0)
+        output_image = Image.fromarray(a)
+        try:
+            output_image.save(output_file_path)
+        except:
+            dir = path.dirname(output_file_path)
+            if not path.exists(dir):
+                makedirs(dir)
+                output_image.save(output_file_path)
+
+
+    # Input and output paths must be directories
+    # Assumes that the top level directory of the input contains a patient name that must be renamed and that all sub directory names and filenames do not contain patient names
+    def anonymize_patient(self, input_path, output_path):
+        debug('Anonymizing patient directory path:  {} ==> {}'.format(input_path, output_path))
+        path_list = os.listdir(input_path)
+
+        for dirname, dirnames, filenames in os.walk(input_path):
+            # follow path to all subdirectories first.
+            for subdirname in dirnames:
+                print(os.path.join(dirname, subdirname))
+
+            # then follow path to all filenames.
+            for filename in filenames:
+                self.anonymize_file(os.path.join(dirname, filename), os.path.join(output_path, filename))
+                # print(os.path.join(dirname, filename))
+
+# Input and output paths must be directories
+def anonymize_path(input_path, output_path):
+    debug('Anonymizing path:  {} ==> {}'.format(input_path, output_path))
+    path_list = os.listdir(input_path)
+
+    for dirname, dirnames, filenames in os.walk(input_path):
+        # follow path to all subdirectories first.
+        for subdirname in dirnames:
+            print(os.path.join(dirname, subdirname))
+
+        # then follow path to all filenames.
+        for filename in filenames:
+            anonymize_file(os.path.join(dirname, filename), os.path.join(output_path, filename))
+            # print(os.path.join(dirname, filename))
+
+
+    # input_image = Image.open(input_file_path)
+    # a = numpy.array(input_image)
+    # a = numpy.delete(a, slice(20), axis=0)
+    # output_image = Image.fromarray(a)
+    # output_image.save(output_file_path)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Vasolabs Controller.",argument_default='start')
-    parser.add_argument('-cfgfile', default='controller.ini', help='Use specified file for configuration.')
-    parser.add_argument('-imagefile', default='image.bmp', help='Use specified image file.')
-    parser.add_argument('action', nargs="?", choices=['start', 'stop', 'restart'], help='Perform the specified action.')
+    parser = argparse.ArgumentParser(description="Vasolabs Controller.",argument_default='anonymize')
+    # parser.add_argument('-cfgfile', default='controller.ini', help='Use specified file for configuration.')
+    # parser.add_argument('-imagefile', default='image.bmp', help='Use specified image file.')
+    # parser.add_argument('-inpath', default='c:\\Users\\jmurray\\imt_studies\\CUPRAK\\Left IMT\\09.39.16 hrs __[0342277].bmp', help='Use specified image file.')
+    parser.add_argument('-inpath', default='c:\\Users\\jmurray\\imt_studies\\CUPRAK', help='Use specified image file.')
+    # parser.add_argument('-outpath', default='c:\\Users\\jmurray\\anon_imt_studies\\anon1.bmp', help='Use specified image file.')
+    parser.add_argument('-outpath', default='c:\\Users\\jmurray\\anon_imt_studies', help='Use specified image file.')
+    # parser.add_argument('-path', default='image.bmp', help='Use specified image file.')
+    parser.add_argument('action', nargs="?", choices=['anonymize', 'anon'], help='Perform the specified action.')
     args = parser.parse_args()
     print(args)
 
@@ -44,8 +109,8 @@ if __name__ == '__main__':
             'file': {
                 'class': 'logging.handlers.RotatingFileHandler',
                 'formatter': 'default',
-                'maxBytes': 100000000,
                 'backupCount': 5,
+                'maxBytes': 100000000,
                 'filename': '0.anoymize.log',
                 'mode': 'w',
             },
@@ -56,25 +121,28 @@ if __name__ == '__main__':
         }
     })
 
-    debug('first debug: {}'.format(5))
     anon = Anon()
-    pseudonym = anon.pseudonym("foozeball")
-    debug("pseudonym: {}".format(pseudonym))
+    # pseudonym = anon.pseudonym("foozeball")
+    # debug("pseudonym: {}".format(pseudonym))
 
     # daemon = ControllerDaemon(args)
-    if 'start' == args.action:
-        debug('second debug: {}'.format(5))
-        im = Image.open(args.imagefile)
-        # im.show()
-        a = numpy.array(im)
-        a = numpy.delete(a, slice(20), axis=0)
-        im = Image.fromarray(a)
-        # im.show()
-        im.save('output.bmp')
+    if 'anonymize' == args.action or 'anon' == args.action:
+        if not path.exists(args.inpath):
+            raise ValueError('Path does not exist: {}'.format(args.inpath))
+        elif not path.exists(path.dirname(args.outpath)):
+            raise ValueError('Path does not exist: {}'.format(args.inpath))
+        elif path.isfile(args.inpath):
+            outpath = args.outpath
+            if path.isdir(outpath):
+                outpath = path.join(outpath, "newfile.bmp")
+            anonymize_file(args.inpath, outpath)
+        
+        ### Process an input directory to an output directory only
+        elif path.isdir(args.inpath) and path.isdir(args.outpath):
+            anon.anonymize_patient(args.inpath, path.join(args.outpath, "patient1"))
 
-    # elif 'stop' == args.action:
-    #     daemon.stop()
-    # elif 'restart' == args.action:
-    #     daemon.restart()
-    # else:
-    #     raise ValueError('unsupported action: {}'.format(args.action))
+        else:
+            raise ValueError('unsupported path arguments: inpath: {}, outpath: {}'.format(args.inpath, args.outpath))
+        
+    else:
+        raise ValueError('unsupported action: {}'.format(args.action))
